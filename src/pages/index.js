e38a3acfa89840af774de8d8c9ca1df2477702e4
Abhishek-Ranabhat <ranabhatabhishek@gmail.com>
2023-07-21 20:50:19 +0545

    css fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import { graphql } from "gatsby";
 
+import "./all.sass";
+
 export default class IndexPage extends React.Component {
   // constructor(props) {
   //   super(props)
