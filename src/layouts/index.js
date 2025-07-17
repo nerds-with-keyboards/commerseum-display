1da76b4cb69d5fd89fe2128a6bd9c9cc044a022e
Floyd Noel <floyd.noel@gmail.com>
2025-07-16 20:01:09 -0400

    small updates

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 
 // import Navbar from '../components/Navbar'
-import Clock from '../components/Clock';
+import Clock from "../components/Clock";
 import "./all.sass";
 
 const TemplateWrapper = ({ children }) => (
